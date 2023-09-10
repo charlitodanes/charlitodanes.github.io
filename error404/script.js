c4ee0a0a26aea0931c839cc9f243a2058fec02fb
Charlito Danes <91553202+charlitodanes@users.noreply.github.com>
2023-09-11 01:36:19 +0800

    Add files via upload

---
+++ error404/script.js
@@ -0,0 +1,5 @@
+/* ----- BACK BUTTON ----- */
+const downloadButton = document.getElementById("back");
+downloadButton.addEventListener("click", function() {
+    window.location.href = "../index.html";
+});
